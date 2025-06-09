06010483f386d4912e247a4b4c7b1959067e809f
thaliadani <thaliadani2@gmail.com>
2025-06-09 16:24:16 -0300

    Commit init

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,17 @@ import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+    private final static String MENSAGEM_NOME = "Ola, informe seu nome: ";
+    private final static String MENSAGEM_IDADE = "Ola, informe sua idade: ";
+
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         var scanner = new Scanner(System.in);
-        System.out.println("Ola, informe seu nome: ");
+        System.out.println(MENSAGEM_NOME);
         var nome = scanner.next();
-        System.out.println("Ola, informe sua idade: ");
+        System.out.println(MENSAGEM_IDADE);
         var idade = scanner.nextInt();
         System.out.printf("Ola %s, sua idade é %s. \n", nome, idade);
     }
